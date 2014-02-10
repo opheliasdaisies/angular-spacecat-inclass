dd4a23f2e5b5a9ea70df84d1669f6df42cee1c7d
ashley williams <heyashleyashley@gmail.com>
2014-02-10 12:51:32 -0500

    add new json, imgs, and angular files for multiviews and routes lesson

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,11 @@
 var spacecatApp = angular.module('spacecatApp', []);
 
 spacecatApp.controller('CatListCtrl', function($scope, $http){
-  $http.get('cats/cats.json').success(function(data) {
+  
+  $http.get('cats/cats.json').success(function(data){
     $scope.cats = data;
   });
- 
+
   $scope.orderProp = 'name';
+
 });
