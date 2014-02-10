5725e56bd8b3bdafe0a3458b8c60570a19bc1a97
Sara Gorecki <sgorecki@gmail.com>
2014-02-10 14:14:48 -0500

    define controller module in controller.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var spacecatControllers = angular.module("spacecatControllers", []);
+
 spacecatApp.controller('CatListCtrl', function($scope, $http){
 
   $http.get('cats/cats.json').success(function(data){
