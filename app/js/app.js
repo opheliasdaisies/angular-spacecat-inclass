4b8537616abf3bbe1b60cdf3783f73c6298576da
Sara Gorecki <sgorecki@gmail.com>
2014-02-10 14:07:07 -0500

    define spacecatApp module in app.js instead of controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,3 @@
 'use strict';
+
+var spacecatApp = angular.module("spacecatApp", []);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var spacecatApp = angular.module("spacecatApp", []);
-
 spacecatApp.controller('CatListCtrl', function($scope, $http){
 
   $http.get('cats/cats.json').success(function(data){
